c9b0825c31251c36dbf09faa07aa0148ee3b34fb
Jhen Anthony Dignos <59014635+jadignos@users.noreply.github.com>
2022-01-20 13:40:58 +0800

    gitignore not specified. delete directory

--- FastwayTrackAndTrace/Model/Response.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FastwayApiManager.Model
-{
-    public class Response
-    {
-        public Result Result { get; set; }
-        public string Generated_In { get; set; } = string.Empty;
-    }
-}
